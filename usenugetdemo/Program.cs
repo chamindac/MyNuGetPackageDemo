2df33227a739d1af563a3c30383b1fa10d2feafa
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2020-12-29 19:47:00 +0530

    usage of nuget package from GitHub

---
+++ usenugetdemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using mynugetdemopkg;
+
+namespace usenugetdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new NugetDemo().HelloWorldNugetDemo());
+        }
+    }
+}
