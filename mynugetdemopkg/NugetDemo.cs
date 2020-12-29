b9b99148a4f4f842447aea8d88a598dcc122ad2c
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2020-12-29 19:10:07 +0530

    Class lib project added

---
+++ mynugetdemopkg/NugetDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mynugetdemopkg
+{
+    public class NugetDemo
+    {
+        public string HelloWorldNugetDemo()
+        {
+            return "Hello world! Welcome to nuget packages in GitHub!";
+        }
+    }
+}
